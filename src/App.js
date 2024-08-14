f7e4f2c93587ad3ccd7835800a976bf2cf067216
Mariam Gamal <mariamgamal4646@gmail.com>
2024-08-14 16:20:50 +0300

    products page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Home from "./pages/Home/Home";
+import Home from "./pages/Products/Products";
 // import Signup from "./pages/signup/signup";
 // import Login from "./pages/login/login";
 
