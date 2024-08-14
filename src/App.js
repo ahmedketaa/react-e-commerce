1ea07b40450935cbcde10c2cede53dd25c842abe
andf.750@gmail.com <andf.750@gmail.com>
2024-08-14 05:39:14 +0300

    protect routes and add footer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./Components/NavBar";
+import React from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
 import Dashboard from "./pages/dashboard/dashboard";
 import Orders from "./pages/dashboard/orders/orders";
 import AddProduct from "./pages/dashboard/addProuduct/addProduct";
 import Customers from "./pages/dashboard/customers/customers";
 import Products from "./pages/dashboard/products/products";
+import NavBar from './Components/NavBar';
+import PrivateRoute from "./protectedRoutes"; 
+import Login from "./pages/login/login";
+import Signup from "./pages/signup/signup";
+import Footer from "./Components/Footer";
+
+function AppContent() {
+  const location = useLocation();
+
+  // Define the routes where the footer should not appear
+  const hideFooterRoutes = ["/login", "/signup", "/dashboard"];
+
+  // Check if the current path matches any of the hideFooterRoutes
+  const shouldHideFooter = hideFooterRoutes.some(route => location.pathname.startsWith(route));
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+
+        <Route path="dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>}>
+          <Route path="products" element={<PrivateRoute><Products /></PrivateRoute>} />
+          <Route path="orders" element={<PrivateRoute><Orders /></PrivateRoute>} />
+          <Route path="addproduct" element={<PrivateRoute><AddProduct /></PrivateRoute>} />
+          <Route path="customers" element={<PrivateRoute><Customers /></PrivateRoute>} />
+        </Route>
+      </Routes>
+      {!shouldHideFooter && <Footer />}
+    </>
+  );
+}
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="dashboard" element={<Dashboard />}>
-            <Route path="products" element={<Products />} />
-            <Route path="orders" element={<Orders />} />
-            <Route path="addproduct" element={<AddProduct />} />
-            <Route path="customers" element={<Customers />} />
-          </Route>
-        </Routes>
-        {/* <NavBar/>
-      <Home/> */}
-        {/* <Signup />
-      <Login /> */}
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   );
 }
 
